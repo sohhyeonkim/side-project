b51eb9af4ba2468c9dd64d42868b651027b6ccc6
sohhyeonkim <sohhyeonkim@gmail.com>
2024-01-22 13:26:24 +0900

    chore: initialize project

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { DataSource } from 'typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env.${process.env.NODE_ENV}`]
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: false,
+      logging: true,
+  }),],
+  controllers: [AppController],
+  providers: [AppService],
+})
+
+
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}
